75813104c4276d4aef62d6a8499c9016ee705317
angel <angel@angel>
2018-10-18 20:50:31 -0400

    ini

--- init_os.sh
+++ init_os.sh
@@ -6,7 +6,7 @@ set -u
 apt update
 
 #
-apt -y install apt-file build-essential curl binutils 
+apt -y install apt-file build-essential curl binutils sudo aptitude
 apt -y install build-essential cmake pkg-config libboost-all-dev libssl-dev 
 apt -y install libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev 
 apt -y install doxygen graphviz libpgm-dev
