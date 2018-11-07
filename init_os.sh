b75b4c4326f566736d5ec95a47e8ed04e83c6de0
angel <zhizhongzhiwai@126.com>
2018-11-07 10:51:39 +0800

    ini

--- init_os.sh
+++ init_os.sh
@@ -6,19 +6,20 @@ set -u
 apt update
 
 #
-apt -y install apt-file build-essential curl binutils sudo aptitude
+apt -y install apt-file build-essential curl binutils sudo aptitude tcpdump
 apt -y install build-essential cmake pkg-config libboost-all-dev libssl-dev 
 apt -y install libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev 
-apt -y install doxygen graphviz libpgm-dev
+apt -y install doxygen graphviz libpgm-dev gdebi
 
 apt -y install build-essential cmake git libgit2-dev clang libncurses5-dev libncursesw5-dev zlib1g-dev pkg-config libssl-dev
+
 
 
 #
 apt -y install python-dev
 pip install -U pip
 
-apt -y install supervisor dnscrypt-proxy fail2ban
+apt -y install supervisor dnscrypt-proxy fail2ban tcpdump
 
 
 apt -y install lsof net-tools screen nload iproute2 
@@ -26,3 +27,9 @@ apt -y install lsof net-tools screen nload iproute2
 #
 localectl set-locale LANG=en_US.UTF-8
 
+
+git config --system alias.co checkout
+git config --system alias.br branch
+git config --system alias.ci commit
+git config --system alias.st status
+git config --system log.date 'format:%Y-%m-%d %H:%M:%S'
