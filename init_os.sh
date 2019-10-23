cdce7c5e9bb566ae8138286a02eb18148bfe0f7e
angel <zhizhongzhiwai@126.com>
2019-10-23 12:47:42 +0800

    fd

--- init_os.sh
+++ init_os.sh
@@ -3,15 +3,15 @@
 set -e
 set -u
 
-dpkg-reconfigure locales
+#dpkg-reconfigure locales
 
 apt update
 
 #
 apt -y install apt-file build-essential curl binutils sudo aptitude tcpdump zile dnsutils
-apt -y install build-essential cmake pkg-config libboost-all-dev libssl-dev 
+apt -y install build-essential cmake pkg-config libboost-all-dev libssl-dev libevent-dev libssl-dev
 apt -y install libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev 
-apt -y install doxygen graphviz libpgm-dev gdebi
+apt -y install doxygen graphviz libpgm-dev gdebi lsof
 
 apt -y install build-essential cmake git libgit2-dev clang libncurses5-dev libncursesw5-dev zlib1g-dev pkg-config libssl-dev
 apt-file update
@@ -23,10 +23,10 @@ curl -o - https://bootstrap.pypa.io/get-pip.py |python
 pip install -U pip 
 pip install toolz 
 
-apt -y install supervisor fail2ban tcpdump libncursesw5-dev gpg dirmngr apt-transport-https source-highlight
+apt -y install supervisor fail2ban tcpdump libncursesw5-dev dirmngr apt-transport-https source-highlight
 apt -y install net-tools
 
-apt -y install lsof net-tools screen nload iproute2 emacs-nox htop less zile
+apt -y install lsof net-tools screen nload iproute2 emacs-nox htop less zile socat netcat
 
 #
 localectl set-locale LANG=en_US.UTF-8
