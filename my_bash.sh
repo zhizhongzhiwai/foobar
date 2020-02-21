186c67f4c86015a98c2b2f07ee303733dc4b82e6
zhizhong <notfast1998@opennumber.org>
2020-02-21 10:42:04 +0800

    update

--- my_bash.sh
+++ my_bash.sh
@@ -1,20 +1,14 @@
 
-export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-export LESS=' -R '
-
-#export EDITOR='emacsclient -t -c'
 export EDITOR='emacs -nw'
-export FIGNORE='.pyc:~:.o:~:#'
+alias ls='ls -F'
 
-alias grep='grep --ignore-case --binary-files=without-match --exclude="*.pyc" --exclude="*.pyo" --exclude="*#" --exclude="*~" --exclude-dir=".*"'
-export GREP_OPTIONS='--ignore-case --binary-files=without-match --exclude="*.pyc" --exclude="*~" --exclude="*#" --exclude-dir=".*"'
-
-alias ls='ls --time-style=iso -F --color=never  --ignore-backups --ignore="*.pyo" --ignore="*.pyc" --ignore="*#"'
-alias less='less -I -N'
 alias lsof='lsof -n -P'
-alias ipython='ipython --no-banner --no-confirm-exit --deep-reload --colors LightBG --pdb'
-alias screen='screen -e^Oo -d -R -l'
+alias ipython='ipython --no-banner --no-confirm-exit'
+alias nload='nload -t 1500'
+alias screen='screen -l -e^Oo'
 alias su='su --login'
+alias sudo='sudo --login'
+
 alias emacs='emacs -nw'
 
 # python
