447b1ca25e9de5eb48418cc4a998fb69919fdefe
zhizhong <notfast1998@opennumber.org>
2020-03-11 12:42:54 +0800

    update

--- init_os.sh
+++ init_os.sh
@@ -11,7 +11,7 @@ apt update
 apt -y install apt-file build-essential curl binutils sudo aptitude tcpdump zile dnsutils locales-all
 apt -y install build-essential cmake pkg-config libboost-all-dev libssl-dev libevent-dev libssl-dev
 apt -y install libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev 
-apt -y install doxygen graphviz libpgm-dev gdebi lsof
+apt -y install doxygen graphviz libpgm-dev lsof
 
 apt -y install build-essential cmake git libgit2-dev clang libncurses5-dev libncursesw5-dev zlib1g-dev pkg-config libssl-dev
 apt-file update
@@ -25,7 +25,7 @@ curl -o - https://bootstrap.pypa.io/get-pip.py |python
 pip install -U pip 
 pip install toolz 
 
-apt -y install supervisor fail2ban tcpdump libncursesw5-dev dirmngr apt-transport-https source-highlight
+apt -y install fail2ban tcpdump libncursesw5-dev dirmngr apt-transport-https source-highlight
 apt -y install net-tools
 
 apt -y install lsof net-tools screen nload iproute2 emacs-nox htop less zile socat netcat
